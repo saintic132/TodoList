83ad1e8a45c028b9ec5959b16ce0f59d6cd78b2a
Ivan Akhremchyk <saintic132@gmail.com>
2021-12-07 15:39:00 +0300

    Add object with tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {v1} from "uuid";
 
 function App() {
+
+    let [tasks, setTasks] = useState([
+        {id: v1(), title: 'HTML', isDone: true},
+        {id: v1(), title: 'CSS', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
+    ]);
+
     return (
         <div className="App">
             <div>
