95662805ad1815796e13a95589f070e2d5049fd4
saintic <saintic132@gmail.com>
2022-02-17 16:53:42 +0300

    Исправленны на иммутабильность state

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -12,12 +12,15 @@ type ActionType = {
 export const userReducer = (state: StateType, action: ActionType) => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            state.age = state.age + 1;
-            return state;
+            return {
+                ...state,
+                age: state.age + 1
+            }
         case 'INCREMENT-CHILDREN-COUNT':
             return {
                 ...state,
-                childrenCount: state.childrenCount +1}
+                childrenCount: state.childrenCount + 1
+            }
         default:
             throw new Error("I don't understand this type")
 
